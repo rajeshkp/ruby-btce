dbd4ff1a584c3d51f257f16c75ffd3da23ab15a1
Rajesh Koilpillai <rajesh.koilpillai@gmail.com>
2015-01-21 14:22:51 +0530

    declared class method

--- lib/btce/api/trade_api.rb
+++ lib/btce/api/trade_api.rb
@@ -61,7 +61,7 @@ module Btce
       @secret = opts[:secret]
     end
 
-    def initialize_api(api_key, api_secret)
+    def self.initialize_api(api_key, api_secret)
       new key: api_key, secret: api_secret
     end
 
